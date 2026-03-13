7e7a8535b051d04bfb129b102dd1de655702cd14
Hafiz Muhammad Adnan Anwar <hafizmadnan567@gmail.com>
2026-03-13 16:27:40 +0500

    js

---
+++ javascript/03_concatenation.js
@@ -0,0 +1,12 @@
+const str = 'Hello' + 'Adnan';
+console.log(str)
+
+
+// console.log('10' + 22)
+console.log(9 - '5')
+console.log('hello' + 'adnan')
+console.log(" " + " ")
+console.log(" " + 0)
+console.log("hello" - "adnan")
+console.log(true + true)
+console.log(true +  false)
