6369a54fff6b9d5397af0cfc6c6b9a6795ffa9dd
Hafiz Muhammad Adnan Anwar <hafizmadnan567@gmail.com>
2026-03-15 15:46:58 +0500

    js

---
+++ javascript/04_expression.js
@@ -0,0 +1,35 @@
+//  Expression
+//  1. Assignment Operator 
+//  Assignment operator in programming are sysmbols used to assign values to variables. 
+// They take the value on the right side of the operator and 
+//  assign it to the variable on the left hand side.
+
+
+var myName = 'adnan'  // in which = sign is assignment operator
+console.log(myName)
+
+
+// 2. Arithmetic operation
+// In programming perform basic mathematical operation
+//  like tha addition , multiplicaiton , subtraction , modulus
+// ADDITION
+var plus = 3+4
+console.log(plus)
+ 
+// SUBTRACTION
+var sub = 9 - 9
+console.log(sub)
+
+// MULTIPLICATION
+var multi = 44*2
+console.log(multi)
+
+// DIVISION
+var divis = 55/ 5
+console.log(divis)
+
+// MOUDULUS
+var modu = 34 % 3
+console.log(modu)
+
+
