07b65a2469708379102832c63d6e41cb3c6b2e60
Hafiz Muhammad Adnan Anwar <hafizmadnan567@gmail.com>
2026-05-01 18:21:55 +0500

    Control_Statement

---
+++ javascript/05_control_statement.js
@@ -0,0 +1,45 @@
+// Control statements in JavaScript
+// if else statement:
+var age = 18;
+if (age >= 18) {
+    console.log("You are an adult.");
+}
+else {
+    console.log("You are a minor.");    
+}
+
+// if else if statement:
+var score = 85;
+if (score >= 90) {
+    console.log("You got an A.");
+}
+else if (score >= 80) {
+    console.log("You got a B.");
+}
+else if (score >= 70) {
+    console.log("You got a C.");
+}
+else {
+    console.log("You got a D.");
+}
+
+
+// while loop:
+var i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+}
+
+// for loop:
+for (var j = 0; j < 5; j++) {
+    console.log(j);
+}
+
+// do while loop:
+var k = 0;
+do {
+    console.log(k);
+    k++;
+}
+while (k < 5);
